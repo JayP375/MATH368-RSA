d513ed310435a58ca0b29f04922564f2fec33bcc
SeanC312 <spc9@comcast.net>
2024-12-01 21:11:52 -0500

    Update Prime.java
    Miller-Rabin developed

--- Prime.java
+++ Prime.java
@@ -31,6 +31,26 @@ public class Prime{
 
     }
 
+    public static BigInteger getA(){ //same process as getBigNumber, just creates a 99 digit number to use as a base for testing
+        
+        StringBuilder numString = new StringBuilder(); //StringBuilder is needed to work with BigInt
+
+        while (true) {
+            int firstNumber = getNumber();
+            if (firstNumber != 0) {
+                numString.append(firstNumber); //make sure number does not start with 0
+                break;
+            }
+        }
+
+        while(numString.length() < 99){
+            numString.append(getNumber());//append random numbers 0-9 until 99 digits are reached
+        }
+
+        return new BigInteger(numString.toString());
+
+    }
+
     public static BigInteger getPrime(BigInteger n){
         BigInteger number = n;
         if(primalityTest(number)){ //if the random number generated is a prime, return that number
@@ -40,17 +60,67 @@ public class Prime{
                 number = number.add(BigInteger.ONE); 
             }
         }
+        System.out.println(number + " is prime!");
         return number;
     }
 
     public static boolean primalityTest(BigInteger n){
 
-        return n.isProbablePrime(100);//built in Miller-Rabin Test from BigInt library
-        //Need to develop this method
+        return millerRabinTest(n);//return t if prime, f otherwise
+
+    }
+
+
+    public static boolean millerRabinTest(BigInteger n){
+        
+        BigInteger d = n.subtract(BigInteger.ONE); //need to represent n-1 as d * 2^r
+
+        int r = 0;
+
+        while(d.mod(BigInteger.TWO).equals(BigInteger.ZERO)){
+
+            d = d.divide(BigInteger.TWO); //Keep dividing n-1 by two
+            r++; //Increment r. 
+            //For Example: 8 -> 4^2 -> 2^3, where the base is d and the exponent is r
+        }
+
+        for(int i = 0; i < 20; i++ ){ //test prime number 20 times
+
+            BigInteger a = getA(); //get a random 99 digit a value
+
+            BigInteger test = a.modPow(d,n); //test a^d mod n
+
+            if (test.equals(BigInteger.ONE)){
+                continue; //if a^d mod n is 1, then it is probably prime - continue testing with new a
+            }
+
+            boolean isComposite = true;
+            for(int j = 0; j < r - 1; j++){ //if a^d mod n did not equal 1, the we square it r-1 times.
+                test = test.modPow(BigInteger.TWO, n);//test = test^2 mod n
+                
+
+                if(test.equals(n.subtract(BigInteger.ONE))){
+                    isComposite = false; //This test is successful, we retest with a new base a
+                    break;
+                }
+
+
+            }
+
+
+            if(isComposite){
+ 
+                return false;
+            }
+        }
+  
+        return true;
 
     }
 
     public static void main(String[] args){
-        System.out.println("The prime number is: " + getPrime(getBigNumber()).toString());
+
+        getPrime(getBigNumber());
+        
     }
 }
